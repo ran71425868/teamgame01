81a26cda1fa8588f77bad801ee9b53ebd3bdb0d8
ran71425868 <2240182@ecc.ac.jp>
2024-10-15 14:32:10 +0900

    shot

---
+++ SourceCode/shot.h
@@ -0,0 +1,17 @@
+#ifndef SHOT_H 
+#define SHOT_H 
+
+//プレイヤーの画像定義
+#define SHOT_TEX_W                 64.0f
+#define SHOT_TEX_H                 64.0f
+#define SHOT_PIVOT_X               32.0f
+#define SHOT_PIVOT_Y               32.0f
+
+
+// 関数のプロトタイプ宣言 
+void shot_init();
+void shot_deinit();
+void shot_update();
+void shot_render();
+
+#endif//SCENE_TITLE_H
