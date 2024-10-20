aa011d07ab5962a273054d517d710bb67ac206a5
Tatutaage1212 <2240181@ecc.ac.jp>
2024-10-20 15:46:11 +0900

    shot

--- SourceCode/judge.cpp
+++ SourceCode/judge.cpp
@@ -57,11 +57,8 @@ void judge()
 
         if (hitCheck(&player_shot, &enemy[i])) {
             enemy[i].moveAlg = -1;
-            
-
-
-
-
+            player_shot.pos.x = 0;
+            player_shot.pos.y = 0;
             game_score();
         }
     }
